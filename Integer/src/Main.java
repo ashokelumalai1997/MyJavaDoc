711038f3ceb8819f023e0d1b390122cdbc075bb5
ASHOK E <ashoke@ASHOKs-Air.alphionsee.in>
2023-09-24 13:28:28 +0530

    String, Character operations

---
+++ Integer/src/Main.java
@@ -0,0 +1,26 @@
+/*
+
+a. Addition and Subtraction: Perform addition and subtraction operations with other integers and store the results in new Integer objects.
+
+b. Multiplication and Division: Perform multiplication and division operations with other integers and store the results in new Integer objects.
+
+c. Conversion to Other Numeric Types: Convert the Integer object to int, double, long, and other numeric types using methods like intValue(), doubleValue(), and longValue().
+
+d. Parsing: Convert a string containing an integer value to an Integer object using the parseInt() method.
+
+e. Comparison: Compare the Integer object with another integer using methods like equals(), compareTo(), and intValue().
+
+f. Bitwise Operations: Perform bitwise operations (AND, OR, XOR, NOT) on the Integer object using methods like &, |, ^, and ~.
+
+g. Math Operations: Use Math class methods like min(), max(), abs(), sqrt(), and pow() in combination with the Integer object.
+
+h. String Representation: Convert the Integer object to a string using the toString() method.
+
+i. Check for Even/Odd: Determine if the Integer object represents an even or odd number using modulo (%) and conditional statements.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
